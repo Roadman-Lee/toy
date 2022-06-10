aaa63fa7492b8aec166b00660e681560f634f696
rdmshine <rdmshine@gmail.com>
2022-06-10 19:32:24 +0900

    accounts app : CustomUserModel modified, date_join delete

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,6 @@ from django.db import models
 class CustomUser(AbstractUser):
     permission_level = models.IntegerField(default=0)
     email = models.EmailField("이메일 주소", max_length=100, unique=True)
-    join_date = models.DateTimeField("가입일", auto_now_add=True)
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
